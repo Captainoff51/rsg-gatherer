e33a2dc2c5e1991915031a985db7b325af74b774
RexShack <rexshackuk@gmail.com>
2022-10-30 12:18:31 +0000

    Initial commit

---
+++ client/client.lua
@@ -0,0 +1,38 @@
+local QRCore = exports['qr-core']:GetCoreObject()
+local trees
+
+---------------------------------------------------------------------------------------
+
+-- woodcutter tree prompts
+Citizen.CreateThread(function()
+    for trees, v in pairs(Config.TreeLocations) do
+        exports['qr-core']:createPrompt(v.treeType, v.coords, QRCore.Shared.Keybinds['J'], 'Start Choping ' .. v.name, {
+            type = 'client',
+            event = 'rsg-gatherer:clent:dowoodcutting',
+            args = {},
+        })
+        if v.showblip == true then
+            local TreeBlip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, v.coords)
+            SetBlipSprite(TreeBlip, 1904459580, 1)
+            SetBlipScale(TreeBlip, 0.2)
+			Citizen.InvokeNative(0x9CB1A1623062F402, TreeBlip, v.name)
+        end
+    end
+end)
+
+-- do woodcutting need axe
+RegisterNetEvent('rsg-gatherer:clent:dowoodcutting')
+AddEventHandler('rsg-gatherer:clent:dowoodcutting', function()
+	local hasItem = QRCore.Functions.HasItem('axe', 1)
+	if hasItem then
+		--TaskStartScenarioInPlace(ped, `EA_WORLD_HUMAN_TREE_CHOP_NEW`, 0, true)
+		TaskStartScenarioInPlace(PlayerPedId(), GetHashKey('EA_WORLD_HUMAN_TREE_CHOP_NEW'), -1, true, false, false, false)
+		Wait(30000)
+		ClearPedTasksImmediately(PlayerPedId(-1))
+		TriggerServerEvent('rsg-gatherer:server:giveWoodReward')
+	else
+		QRCore.Functions.Notify('you don\'t have an axe!', 'error')
+	end
+end)
+
+---------------------------------------------------------------------------------------
---
+++ server/server.lua
@@ -0,0 +1,19 @@
+local QRCore = exports['qr-core']:GetCoreObject()
+
+-- give wood reward
+RegisterServerEvent('rsg-gatherer:server:giveWoodReward')
+AddEventHandler('rsg-gatherer:server:giveWoodReward', function()
+    local src = source
+	local Player = QRCore.Functions.GetPlayer(src)
+	local randomNumber = math.random(1,100)
+	if randomNumber > 80 then
+		TriggerServerEvent('QRCore:Server:RemoveItem', 'axe', 1)
+		TriggerEvent("inventory:client:ItemBox", QRCore.Shared.Items["axe"], "remove")
+		TriggerClientEvent('QRCore:Notify', src, 'your axe is broken!', 'error')
+	else
+		local givewood = math.random(1,3)
+		Player.Functions.AddItem('wood', givewood)
+		TriggerClientEvent('inventory:client:ItemBox', src, QRCore.Shared.Items['wood'], "add")
+		TriggerClientEvent('QRCore:Notify', src, 'you chopped '..givewood..' wood', 'success')
+	end
+end)
